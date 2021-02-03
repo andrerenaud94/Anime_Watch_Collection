cdcd00b720329128af5cb00e426f89c1aaa3ddbf
andre1994 <andregrenaud@gmail.com>
2021-02-03 15:39:18 -0600

    Created a second model

--- main_app/models.py
+++ main_app/models.py
@@ -11,3 +11,23 @@ class Anime(models.Model):
         return self.name
 
 
+MAIN = (
+    ('M', 'Main Story'),
+    ('F', 'Filler')
+)
+
+class Episode(models.Model):
+    number = models.IntegerField()
+    name = models.CharField(max_length=250)
+    eptype = models.CharField(
+        max_length=15,
+        choices=MAIN,
+        default=MAIN[0]
+    )
+    description = models.TextField(max_length=250)
+    link = models.URLField(max_length=250)
+
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'Episode {self.number} titled {self.name}'
