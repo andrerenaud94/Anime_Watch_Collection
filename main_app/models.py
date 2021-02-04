d270d89e8282d259e155164b457c72ab04300acb
andre1994 <andregrenaud@gmail.com>
2021-02-04 16:43:35 -0600

    Adds in basic user model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Anime(models.Model):
@@ -31,3 +32,15 @@ class Episode(models.Model):
 
     def __str__(self):
         return f'Episode {self.number} titled {self.name}'
+
+class User(models.Model):
+    name = models.CharField(max_length=250)
+    username = models.CharField(max_length=250)
+    password = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+
+    animes = models.ManyToManyField(Anime)
+    episodes = models.ManyToManyField(Episode)
+
+    def __str__(self):
+        return f'User: {self.username}'
