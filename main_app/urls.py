8ceaaa2ce391603b398118efa036ee584b1e5211
andre1994 <andregrenaud@gmail.com>
2021-02-06 19:20:39 -0600

    Adds functionality to all buttons. Ties button visibility to users status/rank

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,11 +6,23 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('anime/', views.anime_index, name='anime_index'),
     path('anime/<int:anime_id>', views.anime_detail, name='anime_detail'),
-    path('anime/<int:anime_id>/add_episode', views.add_episode, name='add_episode'),
+    
+    #FAVORITE PATHS
+    path('anime/<int:anime_id>/assoc_anime/', views.assoc_anime, name='assoc_anime'),
+    path('anime/<int:anime_id>/<int:episode_id>/assoc_episode/', views.assoc_episode, name='assoc_episode'),
 
-    #CRUD PATHS
+    #UN-FAVORITE PATHS
+    path('users/profile/<int:user_id>/<int:anime_id>', views.unassoc_anime, name='unassoc_anime'),
+
+    #CRUD PATHS (ANIME)
     path('anime/add_anime', views.add_anime, name='add_anime'),
-    path('anime/<int:anime_id>/edit', views.edit_anime, name='edit_anime'),
+    path('anime/<int:anime_id>/edit/', views.edit_anime, name='edit_anime'),
+    path('anime/<int:anime_id>/delete/', views.delete_anime, name='delete_anime'),
+
+    #CRUD PATHS (EPISODE)
+    path('anime/<int:anime_id>/add_episode', views.add_episode, name='add_episode'),
+    path('anime/<int:anime_id>/<int:episode_id>/edit/', views.edit_episode, name='edit_episode'),
+    path('anime/<int:anime_id>/<int:episode_id>/delete/', views.delete_episode, name='delete_episode'),
 
     #AUTH PATHS
     path('users/profile/', views.profile, name='profile')
--- main_app/views.py
+++ main_app/views.py
@@ -69,9 +69,48 @@ def edit_anime(request, anime_id):
     else:
         form = AnimeForm(instance=anime)
         context = {
-            'form': form
+            'form': form,
+            'anime': anime
         }
     return render(request, 'anime/edit.html', context)
+
+
+
+
+def delete_anime(request, anime_id):
+    anime = Anime.objects.get(id=anime_id)
+    if request.method == 'POST':
+        anime.delete()
+    return redirect('anime_index')
+
+
+
+
+def delete_episode(request, anime_id, episode_id):
+    episode = Episode.objects.get(id=episode_id)
+    if request.method == 'POST':
+        episode.delete()
+    return redirect('anime_detail', anime_id=anime_id)
+
+
+
+
+def edit_episode(request, anime_id, episode_id):
+    episode = Episode.objects.get(id=episode_id)
+    anime = Anime.objects.get(id=anime_id)
+    if request.method == 'POST':
+        form = EpisodeForm(request.POST, instance=episode)
+        if form.is_valid():
+            form.save()
+            return redirect('anime_detail', anime_id=anime_id)
+    else:
+        form = EpisodeForm(instance=episode)
+        context = {
+            'form': form,
+            'anime': anime,
+            'episode': episode
+        }
+    return render(request, 'episode/edit.html', context)
 
 
 
@@ -99,4 +138,28 @@ def add_episode(request, anime_id):
         new_episode.anime_id = anime_id
         new_episode.save()
     
+    return redirect('anime_detail', anime_id=anime_id)
+
+
+
+
+def assoc_anime(request, anime_id):
+    user = request.user
+    user.profile.animes.add(anime_id)
+    return redirect('anime_detail', anime_id=anime_id)
+
+
+
+
+def unassoc_anime(request,user_id, anime_id):
+    user = request.user
+    user.profile.animes.remove(anime_id)
+    return redirect('profile')
+
+
+
+
+def assoc_episode(request, anime_id, episode_id):
+    user = request.user
+    user.profile.episodes.add(episode_id)
     return redirect('anime_detail', anime_id=anime_id)
